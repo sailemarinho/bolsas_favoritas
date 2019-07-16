820e1836944301c67c45ea61ee11548e2122e09b
Saile Marinho saile-ribeiro@hotmail.com <saile-ribeiro@hotmail.com>
2019-07-16 11:39:59 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// Webpack 4 default configuration
+
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+};
